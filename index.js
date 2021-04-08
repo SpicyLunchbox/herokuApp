35e00f7f576dc97458e2428269509c7fdabcc560
Weston Woodard <westonwoodard28@gmail.com>
2021-04-08 09:57:39 -0700

    deploy push

---
+++ index.js
@@ -0,0 +1,22 @@
+const dotenv = require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 9000
+
+app.use('/api/',(_,res)=>{
+    res.json({data:`API is accounted for`})
+})
+app.use(cors());
+app.use(express.json())
+
+
+app.listen(port, () => {
+    console.log(`server is alive on port ${port}`)
+})
+
+// console.log('Houston, we have liftoff')
+// console.log(__dirname)
+// console.log(__filename)
+// console.log(process.env.USERNAME)
+// console.log(process.env.PORT)
